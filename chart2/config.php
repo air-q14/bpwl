6db3ab849f57c7be20d07e4ff80254cd62bdd3a8
fariziti <65403628+fariziti@users.noreply.github.com>
2020-05-16 20:33:03 +0700

    Update config.php

--- chart2/config.php
+++ chart2/config.php
@@ -20,7 +20,7 @@ $databasePassword = '';
 
 $conn = new mysqli($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 if($conn -> connect_errno){
-	echo die ("Gagal Menghubungkan ke Databases ini".$conn->connect_error);
+	echo die ("Gagal Menghubungkan ke Databases".$conn->connect_error);
 }
  
 ?>
