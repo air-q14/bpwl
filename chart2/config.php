6c96f305293536c5e63c70c92afbcd9ae1d8ad97
MuarifAshari <66899803+MuarifAshari@users.noreply.github.com>
2020-06-14 16:08:20 +0700

    Update config.php

--- chart2/config.php
+++ chart2/config.php
@@ -20,7 +20,7 @@ $databasePassword = '';
 
 $conn = new mysqli($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
 if($conn -> connect_errno){
-	echo die ("Gagal Menghubungkan ke Databases".$conn->connect_error);
+	echo die ("Gagal Menghubungkan ke Databases/Lost connection".$conn->connect_error);
 }
  
 ?>
